11689625c6193eb324bafc29339915735d2e4ca8
tarif aljnidi <tarifaljnidi1986@hotmail.com>
2019-04-22 11:26:31 +0100

    load static ressource

---
+++ onlineshopping/src/main/java/net/tarif/onlineshopping/controller/PageController.java
@@ -0,0 +1,23 @@
+package net.tarif.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "welcome to spring mvc");
+		return mv;
+	}
+	@RequestMapping(value = "/test")
+	public ModelAndView test(@RequestParam(value="greeting",required=false)String greeting) {		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;			
+	}	
+}

25768e07528719979967d0f655998d623f26ce52
tarif aljnidi <tarifaljnidi1986@hotmail.com>
2019-04-26 16:25:59 +0100

    using spring form

--- onlineshopping/src/main/java/net/tarif/onlineshopping/controller/ManagementController.java
+++ onlineshopping/src/main/java/net/tarif/onlineshopping/controller/ManagementController.java
@@ -1,6 +1,14 @@
 package net.tarif.onlineshopping.controller;
 
+import java.util.List;
+
+import net.tarif.shoppingbackend.dao.CategoryDAO;
+import net.tarif.shoppingbackend.dto.Category;
+import net.tarif.shoppingbackend.dto.Product;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,6 +16,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/manage")
 public class ManagementController {
+	
+	@Autowired
+	private CategoryDAO categoryDAO;
 
 	@RequestMapping(value="/products", method=RequestMethod.GET)
 	public ModelAndView showManageProducts(){
@@ -15,6 +26,19 @@ public class ManagementController {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("userClickManageProducts",true);
 		mv.addObject("title","Manage Products");
+		
+	Product nProduct = new Product();
+		
+		// assuming that the user is ADMIN
+		// later we will fixed it based on user is SUPPLIER or ADMIN
+		nProduct.setSupplierId(1);
+		nProduct.setActive(true);
+
+		mv.addObject("product", nProduct);
 		return mv;
+	}
+	@ModelAttribute("categories") 
+	public List<Category> getCategories() {
+		return categoryDAO.list();
 	}
 }
